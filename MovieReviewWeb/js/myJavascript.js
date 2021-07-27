1e7610ba7cf282ed732515cc1bb422f0b2ee210b
sx3578426sx <sx3578426sx@gmail.com>
2021-07-27 09:17:40 +0800

    Menu按鈕(回首頁&關於我們)
    新增回首頁跟關於我們的跳轉頁面按鈕

--- MovieReviewWeb/js/myJavascript.js
+++ MovieReviewWeb/js/myJavascript.js
@@ -1,3 +1,19 @@
 $(function(){
+    let Home = $("#Home")
+    let AddMovieBtn = $("#AddMovieBtn");
+    let ReviewBtn = $("#ReviewBtn");
+    let AboutUsBtn = $("#AboutUsBtn");
 
+    Home.on("click", function(){
+        location.href = "http://127.0.0.1:8887/HomePage.html";
+    })
+    AddMovieBtn.on("click", function(){
+        location.href = "";
+    })
+    ReviewBtn.on("click", function(){
+        location.href = "";
+    })
+    AboutUsBtn.on("click", function(){
+        location.href = "http://127.0.0.1:8887/AboutUs.html";
+    })
 })
