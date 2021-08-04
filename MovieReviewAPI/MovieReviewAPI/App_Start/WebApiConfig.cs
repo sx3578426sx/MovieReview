17ba5c44c7ffefc4ccfac156579b8e4ebd1ec5d0
Amy <z851772198@gmail.com>
2021-08-04 21:20:03 +0800

    新增API說明文件&CORS設定
    加入Swagger套件以及API CORS設定

--- MovieReviewAPI/MovieReviewAPI/App_Start/WebApiConfig.cs
+++ MovieReviewAPI/MovieReviewAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
 using System.Web.Http;
 
 namespace MovieReviewAPI
@@ -10,15 +12,21 @@ namespace MovieReviewAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API 設定和服務
+            // 允許XML格式資料序列化
+            config.Formatters.XmlFormatter.UseXmlSerializer = true;
 
             // Web API 路由
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // 設定回傳 format 可以是 json 或 xml
+            config.Formatters.JsonFormatter.AddQueryStringMapping("$format", "json", "application/json");
+            config.Formatters.XmlFormatter.AddQueryStringMapping("$format", "xml", "application/xml");
         }
     }
 }
